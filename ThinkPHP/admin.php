2abc084c2aaba689c3cdb95522dbaf5796df7ffc
AllySu <18795397123@163.com>
2017-12-11 15:28:01 +0800

    thinkPHP

---
+++ ThinkPHP/admin.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @Author: Administrator
+ * @Date:   2017-11-08 14:18:29
+ * @Last Modified by:   Administrator
+ * @Last Modified time: 2017-11-09 17:26:48
+ */
+//common  存放当前项目的公共函数
+//Conf   存放当前项目的配置文件
+//Lang    存放当前项目的语言包
+//Lib     存放当前项目的控制器和模型
+//Runtime     存放当前项目的运行时的文件
+//Tpl         存放当项目录的模板文件
+//  MC存于lib      V存于tpl
+define('APP_NAME', 'Admin');
+define('APP_PATH', './Admin/');
+require('./ThinkPHP/ThinkPHP.php');
+
