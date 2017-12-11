dea6c103260710341f0f7ebaf7879d19cd1c97bc
AllySu <18795397123@163.com>
2017-12-11 15:29:01 +0800

    node.js

---
+++ main/main.js
@@ -0,0 +1,58 @@
+// // 引入 events 模块
+// var events = require('events');
+// // 创建 eventEmitter 对象
+// var eventEmitter = new events.EventEmitter();
+
+// // 创建事件处理程序
+// var connectHandler = function connected() {
+//    console.log('连接成功。');
+
+//    // 触发 data_received 事件
+//    eventEmitter.emit('data_received');
+// }
+
+// // 绑定 connection 事件处理程序
+// eventEmitter.on('connection', connectHandler);
+
+// // 使用匿名函数绑定 data_received 事件
+// eventEmitter.on('data_received', function(){
+//    console.log('数据接收成功。');
+// });
+
+// // 触发 connection 事件
+// eventEmitter.emit('connection');
+
+// console.log("程序执行完毕。");
+//
+//
+//
+//
+//
+/*/---------------------------------------------------------------------------------------
+//* 引入 events 模块
+var events = require('events');
+// 创建 eventEmitter 对象
+var eventEmitter = new events.EventEmitter();
+
+// 绑定 connection 事件处理程序
+eventEmitter.on('connection', function () {
+   console.log('连接成功。');
+
+   // 触发 data_received 事件
+   eventEmitter.emit('data_received');
+});
+
+// 使用匿名函数绑定 data_received 事件
+eventEmitter.on('data_received', function(){
+   console.log('数据接收成功11111111111111111111111111111111111111111111。');
+});
+
+// 触发 connection 事件
+eventEmitter.emit('connection');
+
+console.log("程序执行完毕。");*/
+
+var Hello = require('./hello');
+hello = new Hello();
+hello.setName('BYVoid');
+hello.sayHello();
